0402b4f25c7ee3fa802385c8a7ecf1eaeeb06764
Jonathan Obenland <49774425+Jobenland@users.noreply.github.com>
2020-10-20 00:31:57 -0400

    Add files via upload

---
+++ ParticipationAutoGrader_GUI.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python
+'''
+Participation Auto Grader for INST354
+'''
+import sys
+import tkinter
+from tkinter import ttk
+import pandas as pd
+from difflib import SequenceMatcher
+import statistics as st
+import json
+import time
+import os
+if sys.version_info[0] >= 3:
+    import PySimpleGUI as sg
+else:
+    import PySimpleGUI27 as sg
+
+print = sg.EasyPrint
+
+studentDictionary = {}
+studentArr = []
+sg.theme('DefaultNoMoreNagging')
+
+
+def mainWin():
+    form_rows = [
+        [sg.Text("Fill in the required information and press start")],
+        [sg.Text('Select Class Roster (.txt)', size=(25, 1)),
+         sg.InputText(key='classRoster'), sg.FileBrowse()],
+        [sg.Text('Select Participation File (.xlsx)', size=(25, 1)),
+         sg.InputText(key='partFile'), sg.FileBrowse()],
+        [sg.Button('Start'), sg.Exit()],
+        [sg.Text('                                                                                                      Python 3.x - Jonathan Obenland', text_color='green')]]
+
+    window = sg.Window("Participation Auto Grader")
+    event, values = window.Layout(form_rows).Read()
+
+    # start the event listener
+    while True:
+        if event is None or event == 'Exit':
+            break
+        if event == 'Start':
+            classRoster = values['classRoster']
+            partFile = values['partFile']
+            if classRoster == '' or partFile == '':
+                window.Close()
+                sg.PopupError("Insufficent data or Null Pointers given")
+                mainWin()
+            elif classRoster != '' and partFile != '':
+                window.Close()
+                print("Auto Grader v1.0.0 by Jonathan Obenland")
+                print("Populating class roster...")
+                numStudents = populateDict(classRoster)
+                print("Importing", numStudents,
+                      "students. Gathering peer review data...")
+                gatherReviews(partFile)
+                print("Complete! Awaiting output information...")
+                output = sg.popup_get_folder("Enter Path to Save")
+                os.chdir(output)
+                with open("output.json", "w") as outfile:
+                    json.dump(studentDictionary, outfile)
+                sg.Popup('Complete')
+            break
+
+
+def main():
+    rosterPath = input("Enter txt file containing roster: ")
+    partFile = input("Enter xlsx file contatining participation: ")
+    populateDict(rosterPath)
+    gatherReviews(partFile)
+    with open("output.json", "w") as outfile:
+        json.dump(studentDictionary, outfile)
+
+
+def populateDict(rosterPath):
+    numStudents = 0
+    with open(rosterPath) as file_in:
+        for name in file_in:
+            numStudents += 1
+            name = name.replace("\n", "")
+            studentArr.append(name)
+            studentDictionary[name] = {
+                "Name": name,
+                "Score": -1,
+                "Students": -1,
+                "Raw": [],
+            }
+    return numStudents
+
+
+def gatherReviews(partFile):
+    df = pd.read_excel(partFile)
+    for i in range(46):
+        firstMember = df.iloc[i, 1:10]
+        secondMember = df.iloc[i, 10:19]
+        thirdMember = df.iloc[i, 19:28]
+        fourthMember = df.iloc[i, 28:37]
+        fifthMember = df.iloc[i, 37:46]
+        addToDict(firstMember)
+        addToDict(secondMember)
+        addToDict(thirdMember)
+        addToDict(fourthMember)
+        addToDict(fifthMember)
+
+
+def addToDict(series):
+    try:
+        scoreArr = []
+        studentName = series[0] + " " + series[1]
+        studentName = str(studentName)
+        for index in range(len(studentArr)):
+            similarity = similar(studentArr[index], studentName)
+            if similarity > 0.75:
+                for item in series[2:8]:
+                    scoreArr.append(item)
+                    studentDictionary[studentArr[index]
+                                      ]["Raw"].append(int(item))
+                studentDictionary[studentArr[index]
+                                  ]["Score"] = float(st.mean(scoreArr))
+                studentDictionary[studentArr[index]
+                                  ]["Students"] += 1
+            elif similarity > 0.60:
+                cont = sg.PopupYesNo(
+                    "Is", studentArr[index], "and", studentName, "the same person?")
+                if cont == "Yes":
+                    for item in series[2:8]:
+                        scoreArr.append(item)
+                        studentDictionary[studentArr[index]
+                                          ]["Raw"].append(int(item))
+                    studentDictionary[studentArr[index]
+                                      ]["Score"] = float(st.mean(scoreArr))
+                    studentDictionary[studentArr[index]
+                                      ]["Students"] += 1
+            break
+    except:
+        studentName = "ERROR"
+
+
+def similar(a, b):
+    return SequenceMatcher(None, a, b).ratio()
+
+
+mainWin()
